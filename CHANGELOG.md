0d4e6c59ae72a9734afbe114e5ef673c63faccf8
Brad Wood <woodbr@earlham.edu>
2019-06-20 15:35:15 -0400

    v 1.0.18

--- CHANGELOG.md
+++ CHANGELOG.md
@@ -1,3 +1,7 @@
+### 1.0.18
+
+Refactored Youtube Embeding
+
 ### 1.0.16
 
 Removed slick-theme issue
--- public/js/always.js
+++ public/js/always.js
@@ -4,53 +4,77 @@ apos.define('ec-page-hero-widgets', {
   },
   construct: function(self, options) {
     self.play = function($widget, data, options) {
-        var tag = document.createElement('script');
-        tag.src = "//www.youtube.com/iframe_api";
-        // Inserts YouTube JS code into the page.
-        var firstScriptTag = document.getElementsByTagName('script')[0];
-        firstScriptTag.parentNode.insertBefore(tag, firstScriptTag);
 
-        var player;
-
-        // onYouTubeIframeAPIReady() is called when the IFrame API is ready to go.
-        function onYouTubeIframeAPIReady() {
-          player = new YT.Player('player', {
-            height: '360',
-            width: '640',
-            videoId: data.heroVideo.items[0].video.url.split("v=")[1],
-            playerVars: { 'autohide': 1,'autoplay': 1, 'controls': 0, 'showinfo': 0, 'rel': 0, 'enablejsapi':1, 'wmode' : 'transparent','iv_load_policy':3,'rel': 0,'loop':1,'playlist': '{{videoId}}','iv_load_policy':3,'fs':0},
-            events : {
-              'onReady' : pkOnPlayerReady,
-               'onStateChange' : pkOnPlayerStateChange
-            }
-          });
-        }
-
-        function pkOnPlayerReady(e) {
-          player.mute();
-          e.target.setPlaybackQuality('hd720');
-          player.hideVideoInfo();
-
-        }
-
-        //Load a youtube pixel
-        var pkEnableYoutube = function() {
-          var deferred = jQuery.Deferred();
-          var img = new Image();
-          img.onload = function() { return deferred.resolve(); };
-          img.onerror = function() { return deferred.reject(); };
-          img.src = "//s.ytimg.com/yts/img/pixel-vfl3z5WfW.gif?"+ new Date().getTime();
-          return deferred.promise();
-        };
-
-        //When the video starts to load, set a timer for the video wrap to fade in
-        jQuery(function($){
-          $.when(pkEnableYoutube()).done(function(){
-            setTimeout(function() {
-              $('.video_wrap').fadeIn();
-            }, 2000);
-          });
+var vid =  $widget.find("[data-ec-page-hero-"+data._id+"]").data('videoid'), // "iGpuQ0ioPrM",
+    streams,
+    video_focused = true,
+    video_tag = $("#video"),
+    video_obj = video_tag.get(0);
+$.get("https://images"+~~(Math.random()*33)+"-focus-opensocial.googleusercontent.com/gadgets/proxy?container=none&url=https%3A%2F%2Fwww.youtube.com%2Fget_video_info%3Fvideo_id%3D" + vid, function(data) {
+console.log(data);
+    if (data) {
+        streams = parse_youtube_meta(data);
+        video_tag.attr({
+            src: streams['1080p'] || streams['720p'] || streams['360p']
         });
+
+        document.addEventListener("visibilitychange", function() {
+            video_focused = !video_focused ? video_obj.play() : video_obj.pause();
+        });
+    } else {
+        $('pre').text('Youtube API Error');
+    }
+});
+
+function parse_youtube_meta(rawdata) {
+    var data = parse_str(rawdata),
+        streams = (data.url_encoded_fmt_stream_map + ',' + data.adaptive_fmts).split(','),
+        result = {};
+    streams.forEach(function(s) {
+        var stream = parse_str(s),
+            itag = stream.itag * 1,
+            quality = false,
+            itag_map = {
+                18: '360p',
+                22: '720p',
+                37: '1080p',
+                38: '3072p',
+                82: '360p3d',
+                83: '480p3d',
+                84: '720p3d',
+                85: '1080p3d',
+                133: '240pna',
+                134: '360pna',
+                135: '480pna',
+                136: '720pna',
+                137: '1080pna',
+                264: '1440pna',
+                298: '720p60',
+                299: '1080p60na',
+                160: '144pna',
+                139: "48kbps",
+                140: "128kbps",
+                141: "256kbps"
+            };
+        //if (stream.type.indexOf('o/mp4') > 0) console.log(stream);
+        if (itag_map[itag]) result[itag_map[itag]] = stream.url;
+    });
+    return result;
+};
+
+function parse_str(str) {
+    return str.split('&').reduce(function(params, param) {
+        var paramSplit = param.split('=').map(function(value) {
+            return decodeURIComponent(value.replace('+', ' '));
+        });
+        params[paramSplit[0]] = paramSplit[1];
+        return params;
+    }, {});
+}
+
+
+
+       
         var instance = data._id;
 
         // Get configuration from configuations passed to ec-gallery
